1c2204628a69de9ac220f99b8e6ec36357f53adb
order <jasonlouv@Bifrost-2.local>
2022-07-14 01:45:01 -0500

    Completed

---
+++ cipher.swift
@@ -0,0 +1,18 @@
+var alphabet: [Character] = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
+
+var secretMessage = "Si vis pacem, para bellum"
+
+var message = Array(secretMessage)
+
+for i in 0 ..< message.count {
+    for j in 0 ..< alphabet.count {
+        if message[i] == alphabet[j] {
+            message[i] = alphabet[(j+3) % 26]
+            break
+        }
+    }
+}
+
+print(message)
